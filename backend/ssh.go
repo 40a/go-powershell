052e0a83b58b5f9f5d9cbd684c309e8cd3f2e8b7
Christoph <git@xrstf.de>
2017-02-06 11:33:26 +0100

    fix missing args when using SSH transport, remove hard dependency on crypto/ssh

--- backend/ssh.go
+++ backend/ssh.go
@@ -3,14 +3,26 @@
 package backend
 
 import (
+	"fmt"
 	"io"
+	"regexp"
+	"strings"
 
 	"github.com/juju/errors"
-	"golang.org/x/crypto/ssh"
 )
 
+// sshSession exists so we don't create a hard dependency on crypto/ssh.
+type sshSession interface {
+	Waiter
+
+	StdinPipe() (io.Writer, error)
+	StdoutPipe() (io.Reader, error)
+	StderrPipe() (io.Reader, error)
+	Start(string) error
+}
+
 type SSH struct {
-	Session *ssh.Session
+	Session sshSession
 }
 
 func (b *SSH) StartProcess(cmd string, args ...string) (Waiter, io.Writer, io.Reader, io.Reader, error) {
@@ -29,10 +41,29 @@ func (b *SSH) StartProcess(cmd string, args ...string) (Waiter, io.Writer, io.Re
 		return nil, nil, nil, nil, errors.Annotate(err, "Could not get hold of the SSH session's stderr stream")
 	}
 
-	err = b.Session.Start(cmd) // TODO: quote and add args
+	err = b.Session.Start(b.createCmd(cmd, args))
 	if err != nil {
 		return nil, nil, nil, nil, errors.Annotate(err, "Could not spawn process via SSH")
 	}
 
 	return b.Session, stdin, stdout, stderr, nil
+}
+
+func (b *SSH) createCmd(cmd string, args []string) string {
+	parts := []string{cmd}
+	simple := regexp.MustCompile(`^[a-z0-9_/.~+-]+$`)
+
+	for _, arg := range args {
+		if !simple.MatchString(arg) {
+			arg = b.quote(arg)
+		}
+
+		parts = append(parts, arg)
+	}
+
+	return strings.Join(parts, " ")
+}
+
+func (b *SSH) quote(s string) string {
+	return fmt.Sprintf(`"%s"`, s)
 }
